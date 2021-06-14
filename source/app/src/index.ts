407bc7d432b6c0c3355be8117e5476b0c8595caa
Serabass <serabass565@gmail.com>
2021-06-15 00:28:02 +0600

    1

---
+++ source/app/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+let app = express();
+
+app.get('/', (req, res) => {
+    res.end('Hello from Docker1');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening on http://localhost:${process.env.PORT}.\n Node.js ${process.version}`)
+});
