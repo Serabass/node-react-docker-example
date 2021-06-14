29d3aa719e0d70420f76bb331aceb1ed2e0917fd
Serabass <serabass565@gmail.com>
2021-06-15 01:24:29 +0600

    upd

--- source/app/src/app.ts
+++ source/app/src/app.ts
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.end('Hello from Docker');
 });
 
+app.get('/test', (req, res) => {
+    res.end('Hello test');
+});
+
 app.listen(process.env.PORT, () => {
     console.log(`Server listening on http://localhost:${process.env.PORT}.\n Node.js ${process.version}`)
 });
